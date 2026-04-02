62d44a1b4b0384abc169b93bba73be83534fa8cb
RoryBarnes <rory@astro.washington.edu>
2026-04-02 18:51:12 +0000

    Deploying to gh-pages from @ RoryBarnes/vaibify@76ee2b9f00b33b74baed0369c40aa24a9d3c0a3f 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev186+g602febb59.d20260402',
+    VERSION: '0.1.dev187+g76ee2b9f0.d20260402',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
